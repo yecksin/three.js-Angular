d548edcf91963559b4ed663cb457211459364b14
Yecksin Mauricio <yecksin@gmail.com>
2020-09-25 17:20:41 -0500

    Implementamos three.js, agregamos paginas, footer y header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HouseComponent } from './pages/house/house.component';
+import { CharacterComponent } from './pages/character/character.component';
+import { CameraComponent } from './pages/camera/camera.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HouseComponent,
+    CharacterComponent,
+    CameraComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

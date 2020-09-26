288b3f04ff190d4188c3cf6872f4703b40906cee
Yecksin Mauricio <yecksin@gmail.com>
2020-09-25 20:58:21 -0500

    Header, routing, home three js

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CameraComponent } from './pages/camera/camera.component';
+import { CharacterComponent } from './pages/character/character.component';
+import { HouseComponent } from './pages/house/house.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'camera', component: CameraComponent },
+  { path: 'character', component: CharacterComponent },
+  { path: 'home', component: HomeComponent},
+  { path: 'house', component: HouseComponent },
+  { path: '**', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var THREE;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    setTimeout(() => {
+      this.tres();
+    }, 1000);
+    
+  }
 
   ngOnInit(): void {
   }
+  tres(){
+    console.log("render");
+    var scene3d = document.getElementById("canvas");
+    var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+scene3d.appendChild(renderer.domElement);
+
+var geometry = new THREE.BoxGeometry();
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+var animate = function () {
+  requestAnimationFrame( animate );
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render( scene, camera );
+};
+
+animate();
+}
 }
